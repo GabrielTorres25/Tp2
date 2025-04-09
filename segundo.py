6201f6f8305442db9de02ed4cb6df1c63116466a
Gabriel <gabicjs87@gmail.com>
2025-04-08 21:42:56 -0300

    segundo intento xd

--- segundo.py
+++ segundo.py
@@ -2,3 +2,9 @@ nombre = input("¿Cual es tu nombre?");
 hola = "Hola mundo";
 mensaje = f"{hola}, Bienvenido {nombre}" 
 print (mensaje); 
+
+nombre = input("¿Cual es tu nombre?"); 
+hola = "Hola mundo";
+mensaje = f"{hola}, Bienvenido {nombre}" 
+print (mensaje); 
+
